bb199eda4ef3a6b4949003d35f86ec61a875228e
DenisTolmachev <denis.tolmachev@gmail.com>
2022-04-12 19:15:03 +0300

    task 06 complete

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector('#validation-input');
+const textInputDataLenght = parseInt(textInput.getAttribute('data-length'));
+
+
+
+textInput.addEventListener("blur", (event) => {
+   event.currentTarget.value.length === textInputDataLenght ?  
+   textInput.classList.add('valid') : 
+   textInput.classList.add('invalid')
+console.log('??????????')
+});
