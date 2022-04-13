febc2d7887db35084a594a52d1ce53c777a1c3d4
DenisTolmachev <denis.tolmachev@gmail.com>
2022-04-13 21:00:06 +0300

    task 08 complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+   event.preventDefault();
+   
+   const {
+      elements: {email, password}
+   } = event.currentTarget;
+
+   if (email.value === '' || password.value === '') {
+      return alert('Bсе поля должны быть заполнены!!!')
+   }
+
+   const data = {email:email.value, password:password.value};
+   console.log(data);
+
+   event.currentTarget.reset();
+});
