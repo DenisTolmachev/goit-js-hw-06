4f2b481d85cc3e0906d1187541692b4124a7fc78
DenisTolmachev <denis.tolmachev@gmail.com>
2022-04-17 12:49:45 +0300

    task 09 complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs ={
+   bodyDocument: document.body,
+   colorValue: document.querySelector('.color'),
+   changeColorBtn: document.querySelector('.change-color')
+}
+
+refs.changeColorBtn.addEventListener('click', () => {
+   refs.bodyDocument.style.backgroundColor = getRandomHexColor();
+   refs.colorValue.textContent = getRandomHexColor();
+});
+
+
+
