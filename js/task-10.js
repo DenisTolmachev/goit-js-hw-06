38cd7df4de7cd4bb9b4bfb9446edf509535a8bba
DenisTolmachev <denis.tolmachev@gmail.com>
2022-04-17 15:29:53 +0300

    task 10 complete

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlInput = document.querySelector("#controls>input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxContainer = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const boxes = [];
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = width + "px";
+    box.style.height = height + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    width += 10;
+    height += 10;
+    boxes.push(box);
+  }
+  return boxContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  const allBoxes = boxContainer.querySelectorAll("div");
+  allBoxes.forEach((box) => box.remove());
+}
+
+createBtn.addEventListener("click", () => createBoxes(controlInput.value));
+destroyBtn.addEventListener("click", destroyBoxes);
